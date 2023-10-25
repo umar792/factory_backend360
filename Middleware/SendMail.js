9b98e41656b76a73f7a85937f6496dbcada7d6b2
umar793 <106830172+Umar792@users.noreply.github.com>
2023-10-25 18:20:51 +0500

    new changes

--- Middleware/SendMail.js
+++ Middleware/SendMail.js
@@ -1,26 +1,36 @@
 const nodemailer = require("nodemailer");
 
-const SendMail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
-    service: process.env.SMPT_SERVICE,
-    auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMPT_PASSWORD,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+const SendMail = (options) => {
+  return new Promise(async (resolve, reject) => {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMPT_HOST,
+      port: process.env.SMPT_PORT,
+      service: process.env.SMPT_SERVICE,
+      auth: {
+        user: process.env.SMPT_MAIL,
+        pass: process.env.SMPT_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.SMPT_MAIL,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("Email sent successfully");
+      resolve("Email sent successfully");
+    } catch (error) {
+      console.error("Email not sent:", error);
+      reject(error);
+    }
   });
-
-  const mailoption = {
-    from: process.env.SMPT_MAIL,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-
-  await transporter.sendMail(mailoption);
 };
+
 module.exports = SendMail;
