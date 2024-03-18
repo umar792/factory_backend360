95da10048398de5670808b5b3165f89ab8d58278
umar792 <106830172+umar792@users.noreply.github.com>
2024-03-18 13:34:18 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cloudinary = require("cloudinary");
 
-// ----cors
+// ---------cors
 const cors = require("cors");
 app.use(cors());
 
